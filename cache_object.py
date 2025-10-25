638e9e2826ce9ade1ee83db4a9f94762e25c8f4f
arakshay60 <arakshay60@gmail.com>
2025-10-25 21:44:11 +0530

    Added alert cache

---
+++ cache_object.py
@@ -0,0 +1,15 @@
+import datetime as datetime
+class alertCache:
+    def __init__(self):
+        self.last_threshold=None
+        self.last_week=None
+
+    def current_week(self): #returns the current year and week number as a tuple. For eg (2025,34)
+        today = datetime.datetime.now(datetime.timezone.utc)
+        return today.isocalendar()[:2]
+    
+    def reset_week(self): #resets the last threshold if a new week is detected
+        if self.current_week()!=self.last_week:
+            print('New week detected, resetting last threshold')
+            self.last_week=self.current_week()
+            self.last_threshold=None
--- main.py
+++ main.py
@@ -2,29 +2,33 @@ import time
 import yaml
 from btc_tracker import get_btc_data, is_below_threshold
 from email_notifier import send_email
-
+from cache_object import alertCache
+import random
 def main():
     with open("config.yaml") as f:
         config = yaml.safe_load(f)
 
     interval = config["interval_minutes"] * 60
-    thresholds = sorted(config["alert_threshold_percent"])  # e.g., [5, 10, 20]
+    thresholds = sorted(config["alert_threshold_percent"],reverse=True)  # e.g., [5, 10, 20]
     email_cfg = config["email"]
-
+    alert_cache=alertCache() #object for storing last threshold and last week
     while True:
         btc_data = get_btc_data()
-        price, ath = btc_data["price"], btc_data["ath"]
+        price , ath = btc_data["price"], btc_data["ath"]
         print(f"BTC: ${price} (ATH: ${ath})")
-
+        alert_cache.reset_week() #checks for new week in every iteration
         for threshold in thresholds:
-            if is_below_threshold(price, ath, threshold):
+            #if threshold is greater than last threshold and price is below threshold from ath, send email
+            if threshold > (alert_cache.last_threshold if alert_cache.last_threshold else 0) and is_below_threshold(price, ath, threshold):
                 subject = f"Bitcoin {threshold}% Drop Alert!"
                 body = (
                     f"BTC is at ${price}, which is below {threshold}% "
                     f"from its all-time high of ${ath}."
                 )
-                send_email(email_cfg, subject, body)
-
+                alert_cache.last_threshold=threshold
+                print(f'Sending email for {threshold}% drop')
+                #send_email(email_cfg, subject, body)
+                break
         time.sleep(interval)
 
 if __name__ == "__main__":
