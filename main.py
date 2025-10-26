487b295b835224869b091e9f00a5b50e4144e4a5
arakshay60 <84352956+arakshay60@users.noreply.github.com>
2025-10-26 14:58:39 +0530

    Added alert cache (#2)
    * Added alert cache

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import yaml
 from btc_tracker import get_btc_data, get_percentage_difference_from_ath
 from email_notifier import send_email
+from cache_object import alertCache
 from logger_config import setup_logger
 
 logger = setup_logger(__name__)
@@ -12,9 +13,11 @@ def main():
         config = yaml.safe_load(f)
 
     interval_in_seconds = config["interval_minutes"] * 60
-    thresholds = sorted(config["alert_threshold_percent"])
+    thresholds = sorted(
+        config["alert_threshold_percent"], reverse=True
+    )
     email_cfg = config["email"]
-
+    alert_cache = alertCache()
     logger.info("Starting BTC tracker service...")
 
     while True:
@@ -22,19 +25,20 @@ def main():
             btc_data = get_btc_data()
             price, ath = btc_data["price"], btc_data["ath"]
             logger.info(f"BTC: ${price} (ATH: ${ath})")
-
+            alert_cache.reset_week() 
             for threshold in thresholds:
                 calculated_percentage_difference = get_percentage_difference_from_ath(price, ath)
-                if calculated_percentage_difference >= threshold:
+                if threshold > (
+                alert_cache.last_threshold if alert_cache.last_threshold else 0
+                ) and calculated_percentage_difference >= threshold:
                     logger.info(f"Threshold breached at {calculated_percentage_difference}")
-
                     subject = f"Bitcoin {threshold}% Drop Alert!"
                     body = (
                         f"BTC is at ${price}, which is below {threshold}% "
                         f"from its all-time high of ${ath}."
                         f"Calculated percentage difference is ${calculated_percentage_difference}"
                     )
-
+                    alert_cache.last_threshold = threshold
                     if email_cfg["is_feature_enabled"] is True:
                         logger.info("Email feature enabled, email will be sent for alert")
                         send_email(email_cfg, subject, body)
@@ -50,6 +54,7 @@ def main():
 
         logger.info(f"Sleeping for {interval_in_seconds} seconds")
         time.sleep(interval_in_seconds)
+
 
 
 if __name__ == "__main__":
